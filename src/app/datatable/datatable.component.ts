a670edf3623fbe6ca001582c6274766569e17aa2
“Fernanda <maryfermtzlb@gmail.com>
2024-09-19 19:23:21 -0600

    feat(Datatable): Add Buttons and Icon and Functions

--- src/app/datatable/datatable.component.ts
+++ src/app/datatable/datatable.component.ts
@@ -1,27 +1,21 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { CommonModule } from '@angular/common';
-import { IconDirective, IconSetService, IconModule } from '@coreui/icons-angular';
 import { IconsModule } from '../icons/icons.module';
+import { ButtonConfig } from '../models/interfaces';
 
-export interface PeriodicElement {
-  name: string;
-  age: number;
-  gender: string;
+export interface ColumnDef {
+  columnDef: string;
+  header: string;
+  cell: (element: any) => string | number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { name: 'John', age: 25, gender: 'Male' },
-  { name: 'Jane', age: 30, gender: 'Female' },
-];
-
 @Component({
-  providers: [IconSetService],
   selector: 'app-datatable',
   standalone: true,
   imports: [
@@ -30,19 +24,19 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
-    IconDirective,
-    IconModule,
     IconsModule
   ],
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.scss', '../../scss/buttons.scss'],
 })
 export class DatatableComponent implements AfterViewInit {
-  public icons!: [string, string[]][];
- 
-  displayedColumns: string[] = ['name', 'age', 'gender', 'actions'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  pageSize = 5;
+  @Input() data: any[] = [];
+  @Input() columns: ColumnDef[] = [];
+  @Input() buttons: ButtonConfig[] = [];
+
+  displayedColumns: string[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+  pageSize = 50;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -50,23 +44,15 @@ export class DatatableComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  ngOnChanges() {
+    this.dataSource.data = this.data;
+    this.displayedColumns = this.columns.map(col => col.columnDef).concat('actions');
+    
+  }
+
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value
-      .trim()
-      .toLowerCase();
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
     this.dataSource.filter = filterValue;
   }
 
-  // Métodos para los botones
-  viewElement(element: PeriodicElement) {
-    console.log('Ver:', element);
-  }
-
-  editElement(element: PeriodicElement) {
-    console.log('Editar:', element);
-  }
-
-  deleteElement(element: PeriodicElement) {
-    console.log('Eliminar:', element);
-  }
 }
