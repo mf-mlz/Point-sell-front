eebb6eb9dd5c20483ab81b017b8ee1d4db93b76c
RouterUIZ007 <router.uiz.360@gmail.com>
2024-09-20 14:08:19 -0600

    feat(interfaces): Add Employee

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -25,22 +25,35 @@ export interface KeySat {
 }
 
 // Sale
-
 export interface SaleDate {
   dateBefore: string;
   dateAfter: string;
 }
 
-
 export interface Sale {
   id: number;
-  date: string; 
+  date: string;
   totalAmount: number;
   payment: number;
   dataPayment: string;
   customerId: number;
   employeesId: number;
   status: string;
+  created_at: string;
+  updated_at: string;
+}
+
+// Employees
+export interface Employee {
+  id: number;
+  
+  name: string;
+  email: string;
+  password: string;
+  phone: number;
+  address: string;
+  status: string;
+  role_id: number;
   created_at: string;
   updated_at: string;
 }
