af942209aaccc862ae625e75cfff51e7d4895f92
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 17:14:55 -0600

    feat(Form Validation): Add new Validations

--- src/app/utils/form-validations.ts
+++ src/app/utils/form-validations.ts
@@ -1,30 +1,44 @@
 import { FormGroup } from '@angular/forms';
+import { Injectable } from '@angular/core';
 
-/* Get Error Message */
-export function getErrorMessage(form: FormGroup, field: string): string {
-  const control = form.get(field);
-  
-  if (control?.hasError('required')) {
-    return 'El Campo es Requerido';
-  } else if (control?.hasError('minlength')) {
-    const minLength = control.errors?.['minlength'].requiredLength;
-    return `Ingresa Mínimo ${minLength} Caracteres`;
-  } else if (control?.hasError('minlength')) {
-    const minLength = control.errors?.['minlength'].requiredLength;
-    return `Ingresa Mínimo ${minLength} Caracteres`;
-  }else if (control?.hasError('invalid')) {
-    return `El Campo es Inválido`;
-  } else if (control?.hasError('email')) {
-    return 'Ingresa un correo válido';
-  } else if (control?.hasError('pattern')) {
-    return 'Ingresa únicamente números';
+@Injectable({
+  providedIn: 'root', 
+})
+
+export class ValidationsFormService {
+  /* Get Error Message */
+  getErrorMessage(form: FormGroup, field: string): string {
+    const control = form.get(field);
+
+    if (control?.hasError('required')) {
+      return 'El Campo es Requerido';
+    } else if (control?.hasError('minlength')) {
+      const minLength = control.errors?.['minlength'].requiredLength;
+      return `Ingresa Mínimo ${minLength} Caracteres`;
+    } else if (control?.hasError('minlength')) {
+      const minLength = control.errors?.['minlength'].requiredLength;
+      return `Ingresa Mínimo ${minLength} Caracteres`;
+    } else if (control?.hasError('invalid')) {
+      return `El Campo es Inválido`;
+    } else if (control?.hasError('email')) {
+      return 'Ingresa un correo válido';
+    } else if (control?.hasError('pattern')) {
+      return 'Ingresa únicamente números';
+    } else if (control?.hasError('invalidTotal')) {
+      return 'La Venta no puede ser de $0.00';
+    }
+
+    return '';
   }
-  
-  return '';
-}
 
-/* Verify Invalid Input */
-export function isFieldInvalid(form: FormGroup, field: string): boolean {
-  const control = form.get(field);
-  return !!control && control.invalid && (control.dirty || control.touched);
+  /* Verify Invalid Input */
+  isFieldInvalid(form: FormGroup, field: string): boolean {
+    const control = form.get(field);
+    return !!control && control.invalid && (control.dirty || control.touched);
+  }
+
+  /* Prevent Not Press */
+  preventInput(event: KeyboardEvent | Event): void {
+    event.preventDefault();
+  }
 }
