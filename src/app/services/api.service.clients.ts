c61e109d7dccf6ffa8c5821431529db8f426a19c
“Fernanda <maryfermtzlb@gmail.com>
2024-09-30 17:03:12 -0600

    fix(Api Clients): Change getHeaders => Inyectable Class

--- src/app/services/api.service.clients.ts
+++ src/app/services/api.service.clients.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../services/auth.service';
+import { HttpHeadersService } from './http-headers.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,12 @@ import { AuthService } from '../services/auth.service';
 export class ApiServiceClients {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
-
-  /* Get Headers */
-  private getHeaders(): HttpHeaders {
-    const token = this.authService.getToken();
-    const userPayload = this.authService.getDecodedToken();
-    return new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-      employeeId: userPayload.id,
-    });
-  }
+  constructor(private http: HttpClient, private authService: AuthService, private httpHeadersService: HttpHeadersService) { }
 
   /* Get -- All Clients */
   getAllClients(): Observable<any> {
     const url = `${this.apiUrl}/clients`;
-    const headers = this.getHeaders();
+    const headers = this.httpHeadersService.getHeaders();
     return this.http.get(url, { headers });
   }
 
