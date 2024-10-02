70f94c03ab0f9205cd3d637bd93ee6b2afa74337
“Fernanda <maryfermtzlb@gmail.com>
2024-10-02 14:58:13 -0600

    fix(Auth): Change Cookie => Session Storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,64 +1,40 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { jwtDecode } from 'jwt-decode';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly TOKEN_KEY = environment.nameToken;
-
-  constructor() {}
-
-  public saveToken(token: string): void {
-    localStorage.setItem(this.TOKEN_KEY, token);
+  private token: string = '';
+  constructor(private cookieService: CookieService) {
+    this.token = this.cookieService.get('token');
   }
 
-  public getToken(): string | null {
-    return localStorage.getItem(this.TOKEN_KEY);
-  }
-
-  public removeToken(): void {
-    localStorage.removeItem(this.TOKEN_KEY);
-  }
-
-  public isAuthenticated(): boolean {
-    const token = this.getToken();
-    if (!token) {
-      return false;
-    }
-    console.log(!this.isTokenExpired(token));
-
-    return !this.isTokenExpired(token);
-  }
-
-  private isTokenExpired(token: string): boolean {
-    try {
-      const decoded: any = jwtDecode(token);
-      console.log('Decoded JWT:', decoded);
-
-      if (!decoded.exp) {
-        return true;
-      }
-      const expirationDate = new Date(0);
-      expirationDate.setUTCSeconds(decoded.exp);
-
-      return expirationDate <= new Date();
-    } catch (e) {
-      return true;
-    }
-  }
-
-  public getDecodedToken(): any {
-    const token = this.getToken();
-    if (token) {
+  /* Get Payload => and add in Session Storage */
+  public saveSessionStorage(): any {
+    if (this.token) {
       try {
-        return jwtDecode(token);
-      } catch (e) {
-        return null;
+        const payload: any = jwtDecode(this.token);
+        sessionStorage.setItem('payload', JSON.stringify(payload));
+      } catch (error) {
+        return false;
       }
+    } else {
+      return null;
     }
-    return null;
+  }
+
+  /* Get SessionStorage Payload */
+  public getDecodedToken() {
+    const payload = sessionStorage.getItem('payload');
+    return payload ? JSON.parse(payload) : null;
+  }
+
+  /* Remove SessionStorage => Payload */
+  public clearPayloadFromSession() {
+    this.cookieService.delete('token');
+    sessionStorage.removeItem('payload');
   }
 
 
