6707081d54db4da6bf791bba94d7372a25ce4244
“Fernanda <maryfermtzlb@gmail.com>
2024-10-02 14:56:29 -0600

    feat(Auth Service): Add Session Storage and Change localstorage => cookie

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,17 @@
-import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { jwtDecode } from 'jwt-decode';
 import Swal from 'sweetalert2';
-import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService);
+export const AuthGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const cookieService = inject(CookieService); 
+  const token = cookieService.get('token'); 
+  
 
-  if (authService.isAuthenticated()) {
+  if (isTokenValid(token)) {
     return true;
   } else {
     Swal.fire({
@@ -17,6 +21,22 @@ export const authGuard: CanActivateFn = (route, state) => {
     }).then(() => {
       router.navigate(['/login']);
     });
+    return false;
+  }
+};
+
+const isTokenValid = (token: string): boolean => {
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decoded: any = jwtDecode(token);
+    const expirationDate = decoded.exp * 1000;
+    const now = Date.now();
+
+    return now < expirationDate;
+  } catch (error) {
     return false;
   }
 };
