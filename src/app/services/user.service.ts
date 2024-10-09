c39f8f9ef2815f8a86d6bc0c10b87fe6c1fb166e
“Fernanda <maryfermtzlb@gmail.com>
2024-10-09 14:23:42 -0600

    fix(User Service): Change any for interface loginUser

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service'; 
+import { loginUser } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  public userPayload: any;
+  public userPayload: loginUser;
 
   constructor(private authService: AuthService) {
     this.userPayload = this.authService.getDecodedToken();
