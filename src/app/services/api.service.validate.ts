6f44c150a2626ab4142f6c62b35f1b27243340a1
“Fernanda <maryfermtzlb@gmail.com>
2024-10-09 14:21:54 -0600

    feat(Api Service Validate): New Service => Validate session-employee (Payload Encrypt) for Auth Guard

---
+++ src/app/services/api.service.validate.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthHeaderService } from './auth-header.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceValidate {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient, private authHeaderService: AuthHeaderService) { }
+
+  /* Get  */
+  validate(): Observable<any> {
+    const url = `${this.apiUrl}/validate`;
+    return this.http.get(url, { headers: this.authHeaderService.getHeaders(), withCredentials: true });
+  }
+
+  
+}
