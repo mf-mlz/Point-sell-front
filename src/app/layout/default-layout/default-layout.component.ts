ae5997fb73c7f1c30a466f13f25f12f811d698da
“Fernanda <maryfermtzlb@gmail.com>
2024-10-09 14:19:21 -0600

    fix(Component): Quit console.log

--- src/app/layout/default-layout/default-layout.component.ts
+++ src/app/layout/default-layout/default-layout.component.ts
@@ -52,8 +52,5 @@ export class DefaultLayoutComponent {
   public navItems = navItems;
 
   onScrollbarUpdate($event: any) {
-    // if ($event.verticalUsed) {
-    // console.log('verticalUsed', $event.verticalUsed);
-    // }
   }
 }
