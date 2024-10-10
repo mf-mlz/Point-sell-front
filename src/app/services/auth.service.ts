56196299c3a709e3d84b9b5667fd75f536653a33
“Fernanda <maryfermtzlb@gmail.com>
2024-10-10 17:10:51 -0600

    fix(Auth): Change Service Logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,13 +3,15 @@ import { CookieService } from 'ngx-cookie-service';
 import { loginUserEncrypt } from '../models/interfaces';
 import * as CryptoJS from 'crypto-js';
 import { environment } from '../../environments/environment';
+import { ApiServiceLogout } from './api.service.logout';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private secretKey = environment.secret_key;
-  constructor(private cookieService: CookieService) {}
+  constructor(private cookieService: CookieService, private apiServiceLogout:ApiServiceLogout) {}
 
   /* Get Payload => and add in Session Storage */
   public saveSessionStorage(data: loginUserEncrypt): any {
@@ -45,6 +47,34 @@ export class AuthService {
   public clearPayloadFromSession() {
     sessionStorage.clear();
     this.cookieService.delete('token');
+
+    /* Clear Cookie => Backend */
+    this.apiServiceLogout.logout().subscribe({
+      next: (response) => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          icon: 'success',
+          title: response.message,
+        });
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: error.error?.message || 'Ocurrió un Error',
+        });
+      },
+    });
 
   }
 }
