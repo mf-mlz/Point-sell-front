db36f708c7d387f2cecfc85f56262c932b0ca494
“Fernanda <maryfermtzlb@gmail.com>
2024-10-28 15:47:50 -0600

    feat(Login): Add verify Code and Validate Code

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -27,6 +27,7 @@ import {
 import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
 import { AuthService } from 'src/app/services/auth.service';
+import { VerifyCodeSms } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-login',
@@ -67,14 +68,103 @@ export class LoginComponent {
     });
   }
 
-  onInit(): void{ }
-  
+  onInit(): void {}
+
   onSubmit(): void {
     if (this.loginForm.valid) {
       const credentials = this.loginForm.value;
-  
+
       this.apiService.login(credentials).subscribe({
         next: (response) => {
+          Swal.fire({
+            title: 'Verificación por SMS',
+            html: `<p style="font-size: 15px;">${response.message}</p>`,
+            input: 'text',
+            inputAttributes: {
+              autocapitalize: 'off',
+              required: 'true',
+              placeholder: 'Código de verificación',
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Enviar',
+            showLoaderOnConfirm: true,
+            preConfirm: (code) => {
+              if (!code) {
+                Swal.showValidationMessage(
+                  'Por favor, ingresa el código recibido por SMS.'
+                );
+              }
+              return code;
+            },
+            allowOutsideClick: () => !Swal.isLoading(),
+          }).then((result) => {
+            if (result.isConfirmed) {
+              /* Verify Code */
+              const data = {
+                code: Number(result.value),
+                codeResend: response.code,
+              };
+
+              this.validCode(data)
+                .then((res) => {
+                  const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: true,
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                      toast.onmouseenter = Swal.stopTimer;
+                      toast.onmouseleave = Swal.resumeTimer;
+                    },
+                  });
+                  Toast.fire({
+                    icon: 'success',
+                    title: 'Bienvenido',
+                    text: 'Inicio de Sesión Éxitoso',
+                  });
+                  this.authService.saveSessionStorage(response);
+                  this.router.navigate(['/dashboard']);
+                })
+                .catch((error) => {
+                  const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: true,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                      toast.onmouseenter = Swal.stopTimer;
+                      toast.onmouseleave = Swal.resumeTimer;
+                    },
+                  });
+                  Toast.fire({
+                    icon: 'error',
+                    title: 'Autenticación Fallida',
+                    text: 'El Código de Autenticación ingresado es incorrecto, intentalo de nuevo',
+                  });
+                });
+            } else {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: true,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                },
+              });
+              Toast.fire({
+                icon: 'error',
+                title: 'Acción Cancelada',
+                text: 'La autenticación por SMS es requerida, por favor, intentalo de nuevo.',
+              });
+            }
+          });
+        },
+        error: (error) => {
           const Toast = Swal.mixin({
             toast: true,
             position: 'top-end',
@@ -87,15 +177,6 @@ export class LoginComponent {
             },
           });
           Toast.fire({
-            icon: 'success',
-            title: 'Bienvenido',
-            text: response.message,
-          });
-          this.authService.saveSessionStorage(response);
-          this.router.navigate(['/dashboard']);
-        },
-        error: (error) => {
-          Swal.fire({
             icon: 'error',
             title: 'Error',
             text: error.error?.message || 'Ocurrió un error al Iniciar Sesión.',
@@ -103,11 +184,39 @@ export class LoginComponent {
         },
       });
     } else {
-      Swal.fire({
-        icon: 'warning',
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: true,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: 'error',
         title: 'Error',
-        text: 'Por favor, ingresa correctamente la información.',
+        text: 'Por favor, ingrese correctamente la información',
       });
     }
   }
-}  
+
+  validCode = (data: VerifyCodeSms): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      this.apiService.verifyCode(data).subscribe({
+        next: (response) => {
+          if (response.status) {
+            resolve(true);
+          } else {
+            reject(true);
+          }
+        },
+        error: () => {
+          reject(false);
+        },
+      });
+    });
+  };
+}
